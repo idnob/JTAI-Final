47089e85e91c2f8e54b146fbcc9c68164b0486e5
Andrea Bondi <ironandrea@gmail.com>
2018-04-04 20:47:48 +0200

    Initial Commit

---
+++ JTAI Final/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JTAI Final
+//
+//  Created by Andrea on 04/04/2018.
+//  Copyright © 2018 Andrea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ JTAI FinalTests/JTAI_FinalTests.swift
@@ -0,0 +1,36 @@
+//
+//  JTAI_FinalTests.swift
+//  JTAI FinalTests
+//
+//  Created by Andrea on 04/04/2018.
+//  Copyright © 2018 Andrea. All rights reserved.
+//
+
+import XCTest
+@testable import JTAI_Final
+
+class JTAI_FinalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
